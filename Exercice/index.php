c407a1492285591e7735ed52740ee4e64ba19c2f
gab.caron <gab.mhfu@gmail.com>
2019-01-31 14:45:42 +0100

    Début Exercice

---
+++ Exercice/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>M4105C : Un super module</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <!--
+        <link rel="icon" type="image/jpg" href="" />
+        -->
+        <script
+                src="http://code.jquery.com/jquery-1.12.4.min.js"
+                integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+                crossorigin="anonymous"></script>
+        <script type="text/javascript" src="js/code.js"></script>
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <section>
+        
+        </section>
+        <footer>
+        
+        </footer>
+    </body>
+</html>
